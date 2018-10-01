96d04c32d0a5d5212b679987430f817baa7851f1
Dan Allford <danallford_1@hotmail.com>
2018-10-01 22:26:05 +0100

    implement remove

---
+++ packages/e2e/src/__tests__/remove.tests.ts
@@ -0,0 +1,71 @@
+import { copy, move, readJson, remove } from 'fs-extra'
+import { resolve } from 'path'
+import { exec } from '../utils'
+
+/**
+ * Fixtures directory containing variants of workspace setup to assert against
+ */
+const fixturesDir = resolve(process.cwd(), 'src/__fixtures__')
+
+/**
+ * Create a temporary working directory
+ */
+const cwd = resolve(fixturesDir, 'remove-temp')
+
+beforeEach(async () => {
+    await copy(resolve(fixturesDir, 'clean'), cwd)
+    /**
+     * Clean mono repo doesn't contain a package.json because jest validates module names
+     * and will log a warning if 2 match which they would following the first copy
+     */
+    await move(resolve(cwd, 'package.json.copyme'), resolve(cwd, 'package.json'))
+})
+
+afterEach(async () => {
+    await remove(cwd)
+})
+
+describe('remove command', () => {
+    it('should remove an existing dependency from all workspaces', async () => {
+        await exec('mr add react@16.5.0 deep-freeze', { cwd })
+
+        const { stdout } = await exec('mr remove react', { cwd })
+
+        const workspaceOne = await readJson(resolve(cwd, 'workspaces/workspace-one/package.json'))
+        const workspaceTwo = await readJson(resolve(cwd, 'workspaces/workspace-two/package.json'))
+
+        expect(workspaceOne.dependencies.react).toBe(undefined)
+        expect(workspaceOne.dependencies['deep-freeze']).toBeTruthy()
+        expect(workspaceTwo.dependencies.react).toBe(undefined)
+        expect(workspaceTwo.dependencies['deep-freeze']).toBeTruthy()
+        expect(stdout).toMatchSnapshot()
+    })
+
+    it('should remove an existing dependency from a specified workspace', async () => {
+        await exec('mr add react@16.5.0', { cwd })
+
+        const { stdout } = await exec('mr remove react -w workspace-one', { cwd })
+
+        const workspaceOne = await readJson(resolve(cwd, 'workspaces/workspace-one/package.json'))
+        const workspaceTwo = await readJson(resolve(cwd, 'workspaces/workspace-two/package.json'))
+
+        expect(workspaceOne.dependencies.react).toBe(undefined)
+        expect(workspaceTwo.dependencies.react).toBe('16.5.0')
+        expect(stdout).toMatchSnapshot()
+    })
+
+    it('should remove multiple existing dependencies', async () => {
+        await exec('mr add react@16.5.0 deep-freeze', { cwd })
+
+        const { stdout } = await exec('mr remove react deep-freeze', { cwd })
+
+        const workspaceOne = await readJson(resolve(cwd, 'workspaces/workspace-one/package.json'))
+        const workspaceTwo = await readJson(resolve(cwd, 'workspaces/workspace-two/package.json'))
+
+        expect(workspaceOne.dependencies.react).toBe(undefined)
+        expect(workspaceOne.dependencies['deep-freeze']).toBe(undefined)
+        expect(workspaceTwo.dependencies.react).toBe(undefined)
+        expect(workspaceTwo.dependencies['deep-freeze']).toBe(undefined)
+        expect(stdout).toMatchSnapshot()
+    })
+})
---
+++ packages/mr-yarn/src/commands/remove.ts
@@ -0,0 +1,77 @@
+import { readJson, writeJson } from 'fs-extra'
+import { resolve } from 'path'
+const semverRegex: () => RegExp = require('semver-regex') // tslint:disable-line
+import { Argv } from 'yargs'
+const validateNPMPackageName = require('validate-npm-package-name') // tslint:disable-line
+import { exec } from '../exec'
+import { defaultLogger } from '../logger'
+import { findWorkspacesByName, loadWorkspaces } from '../workspaces'
+
+interface IRemoveCommandOptions {
+    configFilename: string
+    cwd: string
+}
+
+interface IRemoveOptions {
+    configFilename: string
+    cwd: string
+    packages: string[]
+    workspaceSwitch: string
+}
+
+/**
+ * Remove dependencies to workspaces
+ * @param options
+ */
+export const remove = async (options: IRemoveOptions) => {
+    try {
+        /**
+         * Load all workspaces in the mono repo
+         */
+        const monoRepoWorkspaces = await loadWorkspaces(options.cwd, options.configFilename)
+
+        /**
+         * Determine target workspaces
+         */
+        const targetWorkspaces = await findWorkspacesByName(
+            monoRepoWorkspaces,
+            options.workspaceSwitch ? options.workspaceSwitch.split(',') : []
+        )
+
+        defaultLogger.info(`Target workspaces [${targetWorkspaces.map(w => `'${w.name}'`).join(',')}]`)
+        for (const targetWorkspace of targetWorkspaces) {
+            try {
+                await exec(
+                    `yarn remove ${options.packages.join(' ')}`,
+                    {
+                        cwd: targetWorkspace.__workspaceDir
+                    }
+                )
+            } catch (err) {
+                defaultLogger.warn(err)
+            }
+        }
+
+        defaultLogger.info('Done 🎉')
+    } catch (error) {
+        defaultLogger.error(error)
+    }
+}
+
+/**
+ * Apply the 'remove' command to the yargs CLI
+ * @param yargv Instance of yargs CLI
+ */
+export const applyRemoveCommand = (argv: Argv, options: IRemoveCommandOptions) =>
+    argv.command(
+        'remove [packages...]',
+        'Remove packages to workspaces',
+        yargs => yargs,
+        args => {
+            remove({
+                ...options,
+                packages: args.packages,
+                workspaceSwitch: args.workspaces
+            })
+        }
+    )
